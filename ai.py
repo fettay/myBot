d124204252e7cd36b6df13f974b373a4c666ca56
Benjamin Myara <myaraben@gmail.com>
2016-07-13 11:59:48 +0300

    Retrieve misclassified sentences

--- ai.py
+++ ai.py
@@ -67,33 +67,46 @@ def main():
     y = [x.rstrip(' ') for x in y]
     sentences = [clean_string(x[:x.rfind('/'):], stopwords) for x in data]
     # tf-idf
-    tfidf = TfidfVectorizer(min_df=3, stop_words=stopwords, ngram_range=(1, 4))
+    tfidf = TfidfVectorizer(min_df=1, stop_words=stopwords, ngram_range=(1, 2))
     _ = tfidf.fit_transform(sentences)
     idf_ = tfidf.idf_
     representation =  collections.OrderedDict(dict(zip(tfidf.get_feature_names(), idf_)))
     #sorted_representation = sorted(representation.items(), key=operator.itemgetter(1), reverse=True) 
     size_voc = len(representation.keys())
+    #print representation
     print 'Length of vocabulary %i' % (size_voc)
 
-    X = [map_to_representation(sentence, representation, size_voc) for sentence in sentences]
+    #X = [map_to_representation(sentence, representation, size_voc) for sentence in sentences]
 
     acc_mean_test = []
     acc_mean_train = []
-    for i in xrange(1000):
-        print i
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=None)
-        print len(X_train), len(X_test)
+    misclas_sentences = []
+    for i in xrange(450):
+        X_train_, X_test_, y_train, y_test = train_test_split(sentences, y, test_size=0.05, random_state=None)
+        X_train = [map_to_representation(sentence, representation, size_voc) for sentence in X_train_]
+        X_test = [map_to_representation(sentence, representation, size_voc) for sentence in X_test_]
         clf = OneVsRestClassifier(SVC(random_state=0))
         #clf = AdaBoostClassifier(RandomForestClassifier(n_estimators=10))
         clf.fit(X_train, y_train)
-        accuracy_train = accuracy_score(y_train ,clf.predict(X_train))
-        accuracy_test = accuracy_score(y_test ,clf.predict(X_test))
-        print 'Accuracy train is: ', accuracy_train
-        print 'Accuracy test is: ', accuracy_test
+        y_pred_train = clf.predict(X_train)
+        y_pred_test = clf.predict(X_test)
+        accuracy_train = accuracy_score(y_train, y_pred_train)
+        accuracy_test = accuracy_score(y_test, y_pred_test) 
+        #misclas_sentences_ = [(X_test_[i], y_test[i], y_pred_test[i]) for i in xrange(len(y_test)) if y_test[i] != y_pred_test[i]]
+        misclas_sentences_ = [X_test_[i_] for i_ in xrange(len(y_test)) if y_test[i_] != y_pred_test[i_]]
+        misclas_sentences.extend(misclas_sentences_)
+        if i % 100 == 0:
+            print i
+            print len(X_train), len(X_test)
+            print 'Accuracy train is: ', accuracy_train
+            print 'Accuracy test is: ', accuracy_test
         acc_mean_train.append(accuracy_train)
         acc_mean_test.append(accuracy_test)
-    print 'Mean accuracy train: ', np.mean(acc_mean_train)
-    print 'Mean accuracy test: ', np.mean(acc_mean_test)
+    print 'Mean accuracy train: %.3f' % np.mean(acc_mean_train)
+    print 'Mean accuracy test: %.3f' % np.mean(acc_mean_test)
+    print 'List of sentences that were misclassified at least once'
+    print len(list(set(misclas_sentences)))
+    print list(set(misclas_sentences))
 
 
 if __name__ == '__main__':
